5a8073397c7bcdca294e0ba46f508f5695850591
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2020-01-29 10:48:46 +0100

    files: add image files
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- flask_wiki/views.py
+++ flask_wiki/views.py
@@ -9,9 +9,13 @@
 
 """Views to respond to HTTP requests."""
 
-from flask import Blueprint, render_template, current_app, flash, redirect, url_for, request
+import os
+import glob
+from flask import Blueprint, render_template, current_app, flash, redirect, url_for, request, jsonify
 from .api import current_wiki, Processor
 from .forms import EditorForm
+from werkzeug.utils import secure_filename
+
 
 blueprint = Blueprint(
     'wiki',
@@ -52,6 +56,59 @@ def preview():
     data['html'], data['body'], data['meta'], data['toc'] = processor.process()
     return data['html']
 
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@blueprint.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['WIKI_UPLOAD_FOLDER'], filename))
+            return url_for('uploaded_files', filename=filename)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@blueprint.route('/files', methods=['GET', 'POST'])
+def list_files():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['WIKI_UPLOAD_FOLDER'], filename))
+    files = [os.path.basename(f) for f in sorted(glob.glob('/'.join([current_app.config.get('WIKI_UPLOAD_FOLDER'), '*'])), key=os.path.getmtime)]
+    # files = sorted(files, key=os.path.getmtime)
+    return render_template('wiki/files.html', files=files)
 
 @blueprint.errorhandler(404)
 def page_not_found(error):
